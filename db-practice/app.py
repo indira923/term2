ae520c7ceac6c28cd20987156dc27faba88c1820
indira923 <indira.hewitt@csedge.org>
2023-03-11 16:02:16 -0500

    update

--- db-practice/app.py
+++ db-practice/app.py
@@ -1,44 +1,78 @@
-from flask import Flask, render_template, request
+from time import time
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
-import time
+
 from requests import post
+# import time
+# from requests import post
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    user_post = user_post()
+    posts = get_all_posts ()
 
-    return render_template('index.html', posts = post)
+    return render_template('index.html', posts = posts)
 
 @app.route('/success', methods=['POST'])
 def submit():
     name = request.form['name']
-    time = int(request.form['time']) 
-    # height = request.form['height']
-    # group = request.form['group']
-    add_post(name, time, post, )
+    time = (request.form['time']) 
+    post = request.form['posts']
+    add_post(name, time, post)
   
     return render_template('success.html')
 
-def add_post(name, time, post,):
+@app.route('/delete-post/<rowid>')
+def delete(rowid):
+    delete_post(rowid)
+    return redirect(url_for('index'))
+
+
+def delete_post(rowid):
     conn = sqlite3.connect('./static/data/team-edge.db')
     curs = conn.cursor()
-    curs.execute("INSERT INTO write_post (name, time, post) VALUES (?, ?, ?)", (name, time, post, ))
+    curs.execute("DELETE FROM Posts WHERE rowid = ?", (rowid,))
+    conn.commit()
+    conn.close()
+
+@app.route('/edit-post/<rowid>', methods=['POST'])
+def editpost(rowid):
+    name = request.form['name']
+    time = (request.form['time']) 
+    post = request.form['post']
+    update_post(name, time, post, rowid)
+
+    return redirect(url_for('index'))
+
+
+def update_post(name, time, post, rowid):
+    conn = sqlite3.connect('./static/data/team-edge.db')
+    curs = conn.cursor()
+    curs.execute("UPDATE Posts name = ?, time = ?, post = ?, WHERE rowid = ?", (name, time, post, rowid))
+    conn.commit()
+    conn.close()
+
+
+def add_post(name, time, post):
+    conn = sqlite3.connect('./static/data/team-edge.db')
+    curs = conn.cursor()
+    curs.execute("INSERT INTO Posts (name, time, post) VALUES (?, ?, ?)", (name, time, post))
     conn.commit()
     conn.close()
 
 def get_all_posts():
     conn = sqlite3.connect('./static/data/team-edge.db')
     curs = conn.cursor()
-    result = curs.execute("SELECT * FROM posts")
+    result = curs.execute("SELECT rowid, * FROM posts")
     posts = []
 
     for row in result: 
         post = {
-            'name': row[0],
-            'time': row[1],
-            'post': row[2],
+            'name': row[1],
+            'time': row[2],
+            'post': row[3],
+            'rowid' : row[0]
 
         }
         posts.append(post)
